b81bbf6b3b03221a5846ad70ac0a85a5707c9d20
user <user@A24PC5>
2019-06-19 19:49:05 +0200

    this code have some issues

--- Backend/Backend/Controllers/AnimalController.cs
+++ Backend/Backend/Controllers/AnimalController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.DAL.Entities;
 using Backend.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,40 +13,54 @@ namespace Backend.Controllers
     [Route("api/[controller]")]
     public class AnimalController : ControllerBase
     {
-        static List<AnimalViewModel> data = new List<AnimalViewModel>
-            {
-                new AnimalViewModel
-                {
-                    Id = 1,
-                    Name = "Верблюд",
-                    Image = "https://o-prirode.ru/wp-content/uploads/2017/10/verbljud-696x435.jpg",
-                },
-                new AnimalViewModel
-                {
-                    Id = 2,
-                    Name = "Білка",
-                    Image = "http://www.zoolog.com.ua/ssavci/zv.gif",
-                },
-            };
+        //static List<AnimalViewModel> data = new List<AnimalViewModel>
+        //    {
+        //        new AnimalViewModel
+        //        {
+        //            Id = 1,
+        //            Name = "Верблюд",
+        //            Image = "https://o-prirode.ru/wp-content/uploads/2017/10/verbljud-696x435.jpg",
+        //        },
+        //        new AnimalViewModel
+        //        {
+        //            Id = 2,
+        //            Name = "Білка",
+        //            Image = "http://www.zoolog.com.ua/ssavci/zv.gif",
+        //        },
+        //    };
+
+        private readonly EFContext _context;
+        public AnimalController(EFContext context)
+        {
+            _context = context;
+        }
+
         // GET api/animal/search
         [HttpGet("search")]
         public IActionResult Get()
         {
-          
-            return Ok(data);
+            List<AnimalViewModel> model = _context.Animals
+                 .Select(a => new AnimalViewModel
+                 {
+                     Id = a.Id,
+                     Name = a.Name,
+                     Image = a.Image
+                 }).ToList();
+
+            return Ok(model);
         }
 
         // GET api/animal/add
         [HttpPost("add")]
         public IActionResult Post([FromBody]AnimalAddViewModel model)
         {
-            Random rand = new Random();
-            data.Add(new AnimalViewModel
+            //Random rand = new Random();
+            _context.Animals.Add(new DbAnimal
             {
-                Id = rand.Next(10, 33432),
                 Name = model.Name,
                 Image = model.Image
             });
+            _context.SaveChanges();
             return Ok();
         }
 
--- Backend/Backend/Startup.cs
+++ Backend/Backend/Startup.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Backend.DAL.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Backend
 {
@@ -26,12 +32,44 @@ namespace Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
+
+            //string constr = "Server=10.7.0.7; Database=IskandirovCRUD; User Id=novak; Password=1234554321; MultipleActiveResultSets=true";
+                //Configuration
+                //   .GetConnectionString("DefaultConnection");
+            services.AddDbContext<EFContext>(opt =>
+               opt.UseSqlServer("Server=10.7.0.7; Database=IskandirovCRUD; User Id=novak; Password=1234554321; MultipleActiveResultSets=true"));
+
+            services.AddIdentity<DbUser, DbRole>(options => options.Stores.MaxLengthForKeys = 128)
+                .AddEntityFrameworkStores<EFContext>()
+                .AddDefaultTokenProviders();
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = signingKey,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,7 +85,9 @@ namespace Backend
                 .AllowAnyHeader()
                 .AllowAnyMethod());
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseMvc();
+            SeederDB.SeedDataByAS(app.ApplicationServices);
         }
     }
 }
