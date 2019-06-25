4af85dd11616b21d1f42d66edd275507689cad83
Anonymous <user@rivne.itstep.org>
2019-06-25 19:40:35 +0200

    modifi cropper

--- reactByPHP-master/src/components/animal/AddCropper/index.js
+++ reactByPHP-master/src/components/animal/AddCropper/index.js
@@ -1,46 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
+import 'cropperjs/dist/cropper.css';
+import Cropper from 'react-cropper';
 import './index.css';
-
-import CropperWidget from './Modal/index';
+import ImageMapper from 'react-image-mapper';
+import $ from "jquery";
+// import CropperWidget from './modal/index';
 
 class AnimalAddCropperWidgetContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: '',
-            image: '',
+            image: 'https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg',
             src: '',
-            isCropped: false
+            isCropped: false,
         };
     }
     onSubmitForm = (e) => {
         e.preventDefault();
-        console.log('----submit form---');
-
-        const model = { 
-            name: this.state.name,
-            image: this.state.image 
-        };
-        axios.post('http://localhost:100/api/gallery.php', model)
-            .then(
-                (resp)=>{
-                    console.log('--success post--', resp.data);
-                    this.props.history.push('/gallery');
-                },
-                (err) => {
-                    console.log('--err problem---', err);
-                }
-            );
+      
     }
 
     onChangeInput = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
         console.log(e.target.name, e.target.value);
 
     }
 
-    onSelectImage = () =>{
+    onSelectImage = () => {
         console.log('-------Upload image--------');
         this.inputFileElement.click();
     }
@@ -56,8 +45,8 @@ class AnimalAddCropperWidgetContainer extends Component {
             if (files[0].type.match(/^image\//)) {
                 const reader = new FileReader();
                 reader.onload = () => {
-                    //this.toggle(e);
                     this.setState({ src: reader.result, isCropped: true });
+
                 };
                 reader.readAsDataURL(files[0]);
             }
@@ -73,44 +62,116 @@ class AnimalAddCropperWidgetContainer extends Component {
     onCloseCropper = (e) => {
         e.preventDefault();
         this.setState({ isCropped: false });
+        console.log('----submit form---');
+
+        const model = {
+            name: this.state.name,
+            image: this.state.image
+        };
+       
+        axios.post('https://localhost:44368/api/animal/add', model)
+        .then(
+            (resp) => {
+                console.log('--success post--', resp.data);
+                this.props.history.push('/animal');
+            },
+            (err) => {
+                console.log('--err problem---', err);
+            }
+        );
+    }
+    onChangeAngleCropperLeft = (e) => {
+        e.preventDefault();
+        this.setState({ isCropped: false });
+    }
+    onChangeAngleCropperRight = (e) => {
+        e.preventDefault();
+        this.setState({ isCropped: false });
     }
 
-    render() { 
-        const {name, image, src, isCropped }= this.state;
-        
-        return ( 
+    cropImage = (e) => {
+        e.preventDefault();
+        if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
+            return;
+        }
+        this.setState({
+            image: this.cropper.getCroppedCanvas().toDataURL(),
+            isCropped: false,
+        });
+    }
+
+    render() {
+        const { name, image, src, isCropped } = this.state;
+        // base64Img.base64({image}, function(err, data) {
+
+        // })
+        return (
             <React.Fragment>
-                <h1>Додати фото в галерею кропер</h1> 
+                <h1>Додати фото в галерею кропер</h1>
                 <form onSubmit={this.onSubmitForm}>
                     <div className="form-group">
                         <label htmlFor="name">Назва тварини:</label>
-                        <input type="text" 
-                                className="form-control" 
-                                name="name" 
-                                id="name"
-                                onChange={this.onChangeInput}
-                                value={name} />
+                        <input type="text"
+                            className="form-control"
+                            name="name"
+                            id="name"
+                            onChange={this.onChangeInput}
+                            value={name} />
                     </div>
 
                     <div className="form-group" >
-                        <img className="imgUpload" 
-                            onClick={this.onSelectImage} 
-                            src="https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg" />
+                        <img className="imgUpload"
+                            onClick={this.onSelectImage}
+                            src={image} />
 
-                        <input ref={input => this.inputFileElement = input} 
-                                type="file" 
-                                onChange={this.onChangeSelecFile} 
-                                className="d-none" />
-
-                       
-                        <CropperWidget loading={isCropped} src={src} onClose={this.onCloseCropper}/>
+                        <input ref={input => this.inputFileElement = input}
+                            type="file"
+                            onChange={this.onChangeSelecFile}
+                            className="d-none" />
                     </div>
-                    <button type="submit" className="btn btn-info">Додати</button>
                 </form>
+
+                <div className={classnames('croppermodal', { 'open': isCropped })}>
+                    <div className="container">
+                        <div className="row" style={{ marginTop: '100px' }}>
+                            <div className="col-md-8">
+                                <Cropper
+                                    style={{ height: 400, width: '100%' }}
+                                    aspectRatio={1 / 1}
+                                    guides={false}
+                                    viewMode={1}
+                                    preview=".img-preview"
+                                    src={src}
+                                    ref={cropper => { this.cropper = cropper; }}
+                                />
+
+                            </div>
+                            <div className="col-md-4">
+
+                                <div>
+                                    <div className="box" style={{ width: '150%' }}>
+
+                                        <button className="btn btn-success" onClick={this.cropImage}>Обрізати фото</button>
+                                        <button className="btn btn-danger" onClick={this.onCloseCropper}>Закрити</button>
+                                        <button className="btn btn-info" onClick={this.onChangeAngleCropperLeft}>↺</button>
+                                        <button className="btn btn-info" onClick={this.onChangeAngleCropperRight}>↻</button>
+                                        <br />
+                                        <div style={{ marginTop: 20 }}>
+                                            <div className="img-preview" style={{ height: 300 }} />
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
 }
- 
+
 const AnimalAddCropperWidget = AnimalAddCropperWidgetContainer;
 export default AnimalAddCropperWidget;
